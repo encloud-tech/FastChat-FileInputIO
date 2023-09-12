3428ff58ba00a89b0bf732c9f044b63b2634a08a
Deepanshu Arora <deepansharora27@gmail.com>
2023-09-12 13:45:03 +0530

    feature: implement event loop for asynch execution of main()
    Signed-off-by: Deepanshu Arora <deepansharora27@gmail.com>

--- fastchat/serve/cli.py
+++ fastchat/serve/cli.py
@@ -248,6 +248,9 @@ async def main(args):
 
 
 if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
     parser = argparse.ArgumentParser()
     add_model_args(parser)
     parser.add_argument(
